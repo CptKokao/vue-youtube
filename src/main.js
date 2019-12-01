e8b8c52039c89c643485c2d342a0eae1c50dd561
Markov Sergey <markov@qtech.ru>
2019-12-01 19:19:43 +0300

    end lesson7

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Vuelidate from 'vuelidate';
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,14 +7,39 @@ import dateFilter from "@/filters/date.filter";
 import messagePlugin from "@/utils/message.plugin";
 import "materialize-css/dist/js/materialize.min";
 
+// This import loads the firebase namespace.
+import firebase from "firebase/app";
+
+// These imports load individual services into the firebase namespace.
+import "firebase/auth";
+import "firebase/database";
 
 Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 Vue.config.productionTip = false;
-Vue.filter("date", dateFilter)
+Vue.filter("date", dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+firebase.initializeApp({
+  apiKey: "AIzaSyAQj3N0uKpjuRzC7CCk88Ud1eSeqfdEyuw",
+  authDomain: "vue-project-aeae9.firebaseapp.com",
+  databaseURL: "https://vue-project-aeae9.firebaseio.com",
+  projectId: "vue-project-aeae9",
+  storageBucket: "vue-project-aeae9.appspot.com",
+  messagingSenderId: "849389513185",
+  appId: "1:849389513185:web:3f7aa91b1787082e314aa0",
+  measurementId: "G-LF0SLGG785"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+
